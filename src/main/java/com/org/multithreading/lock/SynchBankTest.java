a95603a11ba40363e289bc074b43a79256ded73a
iamsubho76 <iamsubho76@gmail.com>
2017-10-02 16:19:09 +0530

    Lock and Reenterent Lock.

---
+++ src/main/java/com/org/multithreading/lock/SynchBankTest.java
@@ -0,0 +1,17 @@
+package com.org.multithreading.lock;
+
+public class SynchBankTest {
+
+	public static final int NACCOUNTS = 100;
+	public static final double INITIAL_BALANCE = 1000;
+	
+	public static void main(String[] args) {
+		Bank b = new Bank(NACCOUNTS, INITIAL_BALANCE);
+		int i;
+		for (i = 0; i < NACCOUNTS; i++) {
+			Runnable r = new TransferRunnable(b, i, INITIAL_BALANCE);
+			Thread t = new Thread(r);
+			t.start();
+		}
+	}
+}
