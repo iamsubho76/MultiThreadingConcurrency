360fc3048c5f4e6ba01286a808883749ccd2a38e
iamsubho76 <iamsubho76@gmail.com>
2017-12-16 16:47:14 +0530

    DeadLock scenarios

--- src/main/java/com/org/multithreading/basic/deadlock/BankAccountByLock.java
+++ src/main/java/com/org/multithreading/basic/deadlock/BankAccountByLock.java
@@ -15,30 +15,46 @@ public class BankAccountByLock {
 
 	void withdraw(double amount) {
 		// Wait to simulate io like database access ...
+		System.out.println("withdraw method has been invoked");
 		try {
 			Thread.sleep(10l);
 		} catch (InterruptedException e) {
 		}
 		balance -= amount;
+		System.out.printf("withdrawned %10.2f%n", amount);
+		System.out.println("Current balance : " + this.balance);
 	}
 
 	void deposit(double amount) {
 		// Wait to simulate io like database access ...
+		System.out.println("deposit method has been invoked");
 		try {
 			Thread.sleep(10l);
 		} catch (InterruptedException e) {
 		}
 		balance += amount;
+		System.out.printf("deposited %10.2f%n", amount);
+		System.out.println("Current balance : " + this.balance);
 	}
 
 	static void transfer(BankAccountByLock from, BankAccountByLock to, double amount) {
 		from.lock.lock();
-		from.withdraw(amount);
-		to.lock.lock();
-		to.deposit(amount);
-		to.lock.unlock();
+			System.out.printf("Acquired lock to withwrawl from %s to %s  amount : %10.2f%n" , from, to, amount);
+			from.withdraw(amount);
+		//to avoid deadlock uncomment line no. 45 and commented line no. 50
+		//from.lock.unlock();
+			to.lock.lock();
+				System.out.printf("Acquired lock to deposit from %s to %s  amount : %10.2f%n" , from, to, amount);
+				to.deposit(amount);
+			to.lock.unlock();
+		//to replicate the deadlock issue comment line no. 45 and uncommented line no. 50
 		from.lock.unlock();
 	}
+	
+	@Override
+	public String toString() {
+		return Integer.valueOf(this.id).toString();
+	};
 
 	public static void main(String[] args) {
 		final BankAccountByLock fooAccount = new BankAccountByLock(1, 100d);
--- src/main/java/com/org/multithreading/basic/deadlock/BankAccountBySychronization.java
+++ src/main/java/com/org/multithreading/basic/deadlock/BankAccountBySychronization.java
@@ -11,29 +11,43 @@ public class BankAccountBySychronization {
 
 	void withdraw(double amount) {
 		// Wait to simulate io like database access ...
+		System.out.println("withdraw method has been invoked");
 		try {
 			Thread.sleep(10l);
 		} catch (InterruptedException e) {
 		}
 		balance -= amount;
+		System.out.printf("withdrawned %10.2f%n", amount);
+		System.out.println("Current balance : " + this.balance);
 	}
 
 	void deposit(double amount) {
 		// Wait to simulate io like database access ...
+		System.out.println("deposit method has been invoked");
 		try {
 			Thread.sleep(10l);
 		} catch (InterruptedException e) {
 		}
 		balance += amount;
+		System.out.printf("deposited %10.2f%n", amount);
+		System.out.println("Current balance : " + this.balance);
 	}
 
 	static void transfer(BankAccountBySychronization from, BankAccountBySychronization to, double amount) {
 		synchronized (from) {
+			System.out.printf("Acquired lock to withwrawl from %s to %s  amount : %10.2f%n" , from, to, amount);
 			from.withdraw(amount);
+			//to replicate the deadlock issue comment line number 47-50 and uncomment line number 41-44
 			synchronized (to) {
+				System.out.printf("Acquired lock to deposit from %s to %s  amount : %10.2f%n" , from, to, amount);
 				to.deposit(amount);
 			}
 		}
+		//to avoid deadlock uncomment line number 47-50 and comment line number 41-44
+		/*synchronized (to) {
+			System.out.printf("Acquired lock to deposit from %s to %s  amount : %10.2f%n" , from, to, amount);
+			to.deposit(amount);
+		}*/
 	}
 
 	public static void main(String[] args) {
